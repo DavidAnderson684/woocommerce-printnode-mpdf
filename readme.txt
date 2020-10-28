744999869079e5b8ebe556f8ed22d07e2b119caa
David Anderson <DavidAnderson684@users.noreply.github.com>
2020-10-28 14:14:10 +0000

    initial commit

---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== WooCommerce Print Orders mPDF engine for PrintNode ===
+Contributors: David Anderson
+Requires at least: 4.5
+Tested up to: 5.5
+License: MIT
+Requires PHP: 5.6
+
+== Description ==
+
+This plugin makes "WooCommerce Automatic Order Printing (PrintNode)" (https://www.simbahosting.co.uk/s3/product/woocommerce-printnode-automatic-order-printing/) to use mPDF to produce its PDF output, instead of the default DomPDF (at least version 1.4.0 of that plugin is required). It is thus useful for cases that DomPDF has problems with or does not handle, such as right-to-left (RTL) languages.
+
+N.B. This only affects PDFs that the automatic order printing plugin produces internally. For PDFs that were supplied by a separate plugin, you will want to use their accompanying solution (e.g. https://github.com/wpovernight/woocommerce-pdf-ips-mpdf/releases).
+
+PHP >= 5.6.0 is required (reflecting the versions supported by the mPDF library).
+
+== Installation ==
+
+1. Install and then activate this plugin via uploading it into WordPress's plugin installer (use the "Upload" tab). That is all. Once it is active, it is in use (you should be able to see this mentioned in the "Simple summary" section in the PrintNode settings).
+
+== Changelog ==
+
+= 1.0.0 - 2020/10/28 =
+
+* RELEASE: First version
+
+== License ==
+
+Copyright 2020- David Anderson
+
+MIT License:
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ woocommerce-printnode-mpdf.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Plugin Name: WooCommerce PrintNode mPDF engine
+ * Plugin URI: https://github.com/DavidAnderson684/woocommerce-printnode-mpdf
+ * GitHub Plugin URI: https://github.com/DavidAnderson684/woocommerce-printnode-mpdf
+ * Description: Use the mPDF engine for producing PDF files
+ * Version: 1.0.0
+ * Author: David Anderson
+ * Author URI: https://www.simbahosting.co.uk/s3/shop/
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ * Text Domain: woocommerce-printorders
+ */
+
+if (!defined('ABSPATH')) die('No direct access.');
+
+class WooCommerce_Simba_PrintOrders_MPDF {
+
+	// Internal identifier
+	const ID = 'printnode';
+	
+	/**
+	 * Plugin constructor
+	 */
+	public function __construct() {
+		add_filter('woocommerce_printorders_'.$this::ID.'_html_to_pdf_result', array($this, 'html_to_pdf_result'), 10, 3);
+		add_filter('woocommerce_printorders_'.$this::ID.'_internal_options_mpdf_notice', array($this, 'internal_options_mpdf_notice'));
+	}
+
+	/**
+	 * Output visual information in the settings
+	 */
+	public function internal_options_mpdf_notice() {
+		return __('The PDF will be created by the mPDF engine (the associated plugin is installed and active).', 'woocommerce-printorders').'</br>';
+	}
+	
+	/**
+	 * Provide PDF output
+	 *
+	 * @param Boolean|String $result - a PDF stream, or false if not doing anything
+	 * @param String		 $html - input
+	 * @param Array			 $options
+	 *
+	 * @return Boolean|String - filtered output
+	 */
+	public function html_to_pdf_result($result, $html, $options) {
+	
+		// If something else already got in, then pass it through
+		if (false !== $result) return $result;
+	
+		require_once __DIR__ . '/vendor/autoload.php';
+
+		// "Warning: mPDF will clean up old temporary files in the temporary directory. Choose a path dedicated to mPDF only." (https://github.com/mpdf/mpdf)
+		$temp_dir = sys_get_temp_dir().'/mpdf-tmp-'.md5(site_url());
+		
+		$mpdf_options = apply_filters('woocommerce_printorders_'.$this::ID.'_mpdf_options', array(
+			'mode'				=> 'utf-8', 
+			'format'			=> [ round($options['paper_width'] / 2.83464567), round($options['paper_height'] / 2.83464567) ],
+			'tempDir'			=> $temp_dir,
+			'debug'				=> true,
+			'useSubstitutions'	=> file_exists(__DIR__ . '/vendor/mpdf/mpdf/ttfonts/FreeSans.ttf'),
+		));
+
+		error_clear_last();
+		$mpdf = new \Mpdf\Mpdf($mpdf_options);
+		$mpdf->WriteHTML($html);
+
+		return $mpdf->Output(null, \Mpdf\Output\Destination::STRING_RETURN);
+	}
+}
+
+new WooCommerce_Simba_PrintOrders_MPDF();
